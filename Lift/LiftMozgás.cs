fd9cdd68e55bec58737ea5395660e13f3337faa3
zeon77 <zeon77@gmail.com>
2021-06-17 08:35:11 +0200

    Add project files.

---
+++ Lift/LiftMozgás.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lift
+{
+    class LiftMozgás
+    {
+        public DateTime Időpont { get; set; }
+        public int Kárytaszám { get; set; }
+        public int IndulóSzint { get; set; }
+        public int CélSzint { get; set; }
+
+        public LiftMozgás(string sor)
+        {
+            string[] Splitted = sor.Split(' ');
+            Időpont = DateTime.Parse(Splitted[0]);
+            Kárytaszám = int.Parse(Splitted[1]);
+            IndulóSzint = int.Parse(Splitted[2]);
+            CélSzint = int.Parse(Splitted[3]);
+        }
+    }
+}
